68e20244c5dc6bfc7c20653538fc79ed7ef554d1
Adie Williams <hello@adie.codes>
2018-02-16 14:33:08 +0000

    feat: Add comment model

---
+++ models/comments.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const CommentSchema = new Schema({
+  body: {
+    type: String,
+    required: true
+  },
+  belongs_to: {
+    type: Schema.Types.ObjectId,
+    required: true
+  },
+  created_at: {
+    type: Number,
+    default: new Date().getTime()
+  },
+  votes: {
+    type: Number,
+    default: 0
+  },
+  created_by: {
+    type: String,
+    required: true,
+    default: 'northcoder'
+  }
+});
+
+module.exports = mongoose.model('comments', CommentSchema);
