8893cd9ebc3266f6a8cf6a1ded2b9436639d39ff
Adie Williams <hello@adie.codes>
2018-02-17 10:08:59 +0000

    chore: Tidy up describe blocks

--- spec/api.spec.js
+++ spec/api.spec.js
@@ -23,41 +23,39 @@ describe('API', () => {
       .catch(console.log);
   });
 
-  describe('#API', () => {
-    describe('GET /api/topics', () => {
-      it('returns 200 and all topics', () => {
-        return request
-          .get('/api/topics')
-          .expect(200)
-          .then(res => {
-            expect(res.body.topics.length).to.eql(usefulData.topics.length);
-            expect(res.body.topics[0].title).to.be.a('string');
-            expect(res.body.topics[0].slug).to.be.a('string');
-            expect(res.body.topics[0]._id).to.be.a('string');
-          });
-      });
+  describe('GET /api/topics', () => {
+    it('returns 200 and all topics', () => {
+      return request
+        .get('/api/topics')
+        .expect(200)
+        .then(res => {
+          expect(res.body.topics.length).to.eql(usefulData.topics.length);
+          expect(res.body.topics[0].title).to.be.a('string');
+          expect(res.body.topics[0].slug).to.be.a('string');
+          expect(res.body.topics[0]._id).to.be.a('string');
+        });
     });
-    describe('GET /api/topics/:topid/articles', () => {
-      it('returns 200 and all articles for matching topic', () => {
-        return request
-          .get(`/api/topics/football/articles`)
-          .expect(200)
-          .then(res => {
-            expect(res.body.articles.length).to.equal(1);
-            expect(res.body.articles[0].title).to.be.a('string');
-            expect(res.body.articles[0].body).to.be.a('string');
-            expect(res.body.articles[0].votes).to.be.a('number');
-          });
-      });
-      it('returns 404 for invalid topic', () => {
-        return request
-          .get(`/api/topics/tennis/articles`)
-          .expect(404)
-          .then(res => {
-            expect(res.body.status).to.equal(404);
-            expect(res.body.msg).to.equal('No content');
-          });
-      });
+  });
+  describe('GET /api/topics/:topid/articles', () => {
+    it('returns 200 and all articles for matching topic', () => {
+      return request
+        .get(`/api/topics/football/articles`)
+        .expect(200)
+        .then(res => {
+          expect(res.body.articles.length).to.equal(1);
+          expect(res.body.articles[0].title).to.be.a('string');
+          expect(res.body.articles[0].body).to.be.a('string');
+          expect(res.body.articles[0].votes).to.be.a('number');
+        });
+    });
+    it('returns 404 for invalid topic', () => {
+      return request
+        .get(`/api/topics/tennis/articles`)
+        .expect(404)
+        .then(res => {
+          expect(res.body.status).to.equal(404);
+          expect(res.body.msg).to.equal('No content');
+        });
     });
   });
   describe('GET /api/articles', () => {
@@ -74,15 +72,15 @@ describe('API', () => {
         });
     });
   });
-  describe('#server', () => {
-    it('complete final check and disconnect', () => {
-      return request
-        .get('/')
-        .expect(200)
-        .then(res => {
-          expect(res.body.msg).to.be.equal('Server running on port 3090');
-          mongoose.disconnect();
-        });
-    });
+});
+describe('#server', () => {
+  it('complete final check and disconnect', () => {
+    return request
+      .get('/')
+      .expect(200)
+      .then(res => {
+        expect(res.body.msg).to.be.equal('Server running on port 3090');
+        mongoose.disconnect();
+      });
   });
 });
