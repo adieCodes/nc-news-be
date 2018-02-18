57798f6492fa0b76726530a235027a8486431391
Adie Williams <hello@adie.codes>
2018-02-18 14:06:28 +0000

    feat: Add controller for POST /api/articles/:articleId/comments

--- controllers/comments.controller.js
+++ controllers/comments.controller.js
@@ -12,4 +12,18 @@ const getCommentsByArticleId = (req, res, next) => {
     });
 };
 
-module.exports = { getCommentsByArticleId };
+const addCommentToArticle = (req, res, next) => {
+  const { articleId } = req.params;
+  const newComment = new Comment({ body: req.body.comment, belongs_to: articleId });
+
+  return newComment
+    .save()
+    .then(() => Comment.find({ belongs_to: articleId }))
+    .then(comments => res.status(201).send({ comments }))
+    .catch(err => {
+      if (err.name === 'ValidationError') return next({ status: 400, msg: 'Invalid article Id' });
+      return next({ err });
+    });
+};
+
+module.exports = { getCommentsByArticleId, addCommentToArticle };
