a19e3af14d21445b958074fe0dc7fe29c5d94613
Adie Williams <hello@adie.codes>
2018-02-18 14:06:04 +0000

    feat: Add routes for POST /api/articles/:articleId/comments

--- routes/articles.router.js
+++ routes/articles.router.js
@@ -1,9 +1,13 @@
 const router = require('express').Router();
 const { getAllArticles, getArticleById } = require('../controllers/articles.controller');
-const { getCommentsByArticleId } = require('../controllers/comments.controller');
+const {
+  getCommentsByArticleId,
+  addCommentToArticle
+} = require('../controllers/comments.controller');
 
 router.get('/', getAllArticles);
 router.get('/:articleId', getArticleById);
 router.get('/:articleId/comments', getCommentsByArticleId);
+router.post('/:articleId/comments', addCommentToArticle);
 
 module.exports = router;
