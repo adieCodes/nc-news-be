7a8fdf12c516bb83289196c19ddb6fba9b5d0a4d
Adie Williams <hello@adie.codes>
2018-02-18 16:04:38 +0000

    feat: Add route for DELETE /api/comments/:commentId

--- routes/comments.routes.js
+++ routes/comments.routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
-const { changeVoteCount } = require('../controllers/comments.controller');
+const { changeVoteCount, deleteComment } = require('../controllers/comments.controller');
 
 router.put('/:commentId', changeVoteCount);
+router.delete('/:commentId', deleteComment);
 
 module.exports = router;
