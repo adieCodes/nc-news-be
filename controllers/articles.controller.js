2d52ff1e45daf5702bddacd63749dc2e2cf673f4
Adie Williams <hello@adie.codes>
2018-02-18 14:51:50 +0000

    feat: Add controller for PUT /api/articles/:articleId

--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -26,4 +26,16 @@ const getArticleById = (req, res, next) => {
     });
 };
 
-module.exports = { getAllArticles, getTopicArticles, getArticleById };
+const changeVoteCount = (req, res, next) => {
+  const { articleId } = req.params;
+  const changeVoteCountBy = req.query.vote === 'up' ? 1 : -1;
+
+  Article.findByIdAndUpdate(articleId, { $inc: { votes: changeVoteCountBy } }, { new: true })
+    .then(article => res.status(200).send({ article }))
+    .catch(err => {
+      if (err.name === 'CastError') return next({ status: 400, msg: 'Invalid Article ID' });
+      return next(err);
+    });
+};
+
+module.exports = { getAllArticles, getTopicArticles, getArticleById, changeVoteCount };
