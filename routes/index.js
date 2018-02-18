426caaaa7f9521fedafc2f9663ec65ab5f8b8f83
Adie Williams <hello@adie.codes>
2018-02-18 15:34:22 +0000

    feat: Add routes for PUT /api/comments/:commentId

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const topicsRouter = require('./topics.router');
 const articlesRouter = require('./articles.router');
+const commentsRouter = require('./comments.routes');
 
 router.use('/topics', topicsRouter);
 router.use('/articles', articlesRouter);
+router.use('/comments', commentsRouter);
 
 module.exports = router;
