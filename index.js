7a5dc775047f51e9d7c798f5575245f16fdf9fd6
Adie Williams <hello@adie.codes>
2018-03-04 15:34:40 +0000

    fix: Access port properly via config or env vars

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 /* eslint-disable no-console */
 
 const app = require('./server');
+const config = require('./config');
 
-const { PORT } = process.env;
+const PORT = config.PORT[process.env.NODE_ENV] || process.env.PORT;
 
 app.listen(PORT, err => {
   if (err) console.log(err);
