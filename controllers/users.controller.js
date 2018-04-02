8fcc8e4d69283276d0abf79d24f920ae1765942c
Adie Williams <hello@adie.codes>
2018-04-02 11:02:47 +0100

    feat: Return userArticles and Comments with UserData (#30)

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,12 +1,20 @@
-const { User } = require('../models');
+const { Article, Comment, User } = require('../models');
 
 const getUser = (req, res, next) => {
   const { username } = req.params;
+  const userDataQuery = User.findOne({ username });
+  const userArticleQuery = Article.find({ created_by: username });
+  const userCommentQuery = Comment.find({ created_by: username });
 
-  User.findOne({ username })
-    .then(user => {
-      if (!user) return next({ status: 400, msg: `No-one is using the username ${username}` });
-      return res.status(200).send({ user });
+  return Promise.all([userDataQuery, userArticleQuery, userCommentQuery])
+    .then(queryResults => {
+      const userData = queryResults[0];
+      const userArticles = queryResults[1];
+      const userComments = queryResults[2];
+
+      if (!userData) return next({ status: 400, msg: `No-one is using the username ${username}` });
+
+      return res.status(200).send({ userData, userArticles, userComments });
     })
     .catch(next);
 };
